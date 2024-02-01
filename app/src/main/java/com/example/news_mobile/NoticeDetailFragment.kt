76a817bd68d0a55ac7e084236410c6d5ccd84f3b
John <jolivares1700@alumno.ipn.mx>
2024-02-01 11:41:22 -0600

    res/layout: Creación y Diseño de Fragment_notice. Creación y Diseño de Fragment_noticeDetail.
    res/drawable:
    Creación de un gradient para el fondo.
    Creación de vectores:
    Bookmark
    Calendar
    Favourite
    
    res/navigation:
    Se agregó el grafico de navegación entre fragmentos.

---
+++ app/src/main/java/com/example/news_mobile/NoticeDetailFragment.kt
@@ -0,0 +1,14 @@
+package com.example.news_mobile
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class NoticeDetailFragment : Fragment(R.layout.fragment_notice_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
