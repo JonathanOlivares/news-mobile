584c3c5a16689095988a57ba9b2a9ee7f485f5a3
John <jolivares1700@alumno.ipn.mx>
2024-02-01 18:09:39 -0600

    java: Desarrollo de la capa data que es: Notice: dentro de este se tienen dos clase. 1. la data class que contiene los objetos que se obtendran del servidor con su tipo de dato.
    2.La lista que retornará del servidor que se le dará a la clase de los objetos (NoticeList).
    
    NoticeDataSource:
    Creación del método TechCrunchNotice que retornará la lista obtenida del servidor a la segunda clase de Notice (NoticeList)

--- app/src/main/java/com/example/news_mobile/data/model/Notice.kt
+++ app/src/main/java/com/example/news_mobile/data/model/Notice.kt
@@ -1,4 +1,17 @@
 package com.example.news_mobile.data.model
 
-data class Notice(val id: Int = -1) {
-}
+data class Notice(
+    val author: String = "",
+    val title: String = "",
+    val description: String = "",
+    val url: String = "",
+    val urlToImage: String = "",
+    val publishedAt: String = "",
+    val content: String = "")
+
+
+//Data class que corresponde a cada uno de los objetos (que se crearon en la clase Notice) en una
+//lista llamada articles la cual debe llamarse igual a la que da la API porque sino, no se podrá
+// traer los datos del servidor.
+//Es una lista de noticias List<Notice>
+data class NoticeList(val articles: List<Notice> = listOf())
--- app/src/main/java/com/example/news_mobile/data/remote/NoticeDataSource.kt
+++ app/src/main/java/com/example/news_mobile/data/remote/NoticeDataSource.kt
@@ -1,4 +1,12 @@
 package com.example.news_mobile.data.remote
 
+import com.example.news_mobile.data.model.NoticeList
+
+
 class NoticeDataSource {
+
+   fun  TechCrunchNotice (): NoticeList{
+       return NoticeList()
+   }
+
 }
